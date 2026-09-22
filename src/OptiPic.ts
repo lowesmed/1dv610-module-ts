e112a9c8b7b8a52c8d845b770d2627f95e59f6d9
Lowe Smed <ls222cg@student.lnu.se>
2026-09-22 16:52:46 +0200

    Create OptiPic class, remove template code & edit readme

---
+++ src/OptiPic.ts
@@ -0,0 +1,27 @@
+/**
+ * @file Module for the class OptiPic.
+ * @module src/OptiPic
+ * @author Lowe Smed <ls222cg@student.lnu.se>
+ * @version 1.0.0
+ */
+
+
+/**
+ * Represents the OptiPic
+ */
+export class OptiPic {
+  /**
+   * Path of image to optimize.
+   */
+  #image
+
+  /**
+   * Initiziates OptiPic.
+   *
+   * @param path Path to image
+   */
+  constructor (path: string) {
+    this.#image = path
+    console.log(this.#image)
+  }
+}
